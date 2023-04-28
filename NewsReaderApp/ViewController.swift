8aa3dadb8fb1cba76c1808fd85390afd82346759
Fikri Bill Gufran <billgufran@gmail.com>
2023-04-28 08:58:34 +0800

    Initial Commit

---
+++ NewsReaderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsReaderApp
+//
+//  Created by Muhammad Fikri Bill Gufran on 28/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

0eaf4497459366994eb470c6f4d36da612b653cb
Fikri Bill Gufran <billgufran@gmail.com>
2023-04-28 14:20:04 +0800

    feat: setup login page layout

--- NewsReaderApp/ViewController.swift
+++ NewsReaderApp/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailTitleLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTitleLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        emailTitleLabel.text = "Surel"
+        emailTextField.placeholder = "surel@pengguna.com"
+        
+        passwordTitleLabel.text = "Kata Sandi"
+        passwordTextField.placeholder = "Rahasia"
+        
+        loginButton.setTitle("Masuk", for: UIControl.State.normal)
+        loginButton.layer.cornerRadius = 4
     }
-
-
+    
+    
+    @IBAction func loginButtonOnTap(_ sender: Any) {
+        print("Email: \(emailTextField.text ?? "-")")
+    }
 }
 

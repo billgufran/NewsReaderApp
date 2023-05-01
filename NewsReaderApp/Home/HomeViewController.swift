0033923cf7083ab5086a8abf078a9e281ca5506d
Fikri Bill Gufran <billgufran@gmail.com>
2023-05-01 22:21:25 +0800

    feat: refresh controller

--- NewsReaderApp/Home/HomeViewController.swift
+++ NewsReaderApp/Home/HomeViewController.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 import SDWebImage
+import SafariServices
 
 // MARK: - UIViewController
 class HomeViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    weak var refreshControl: UIRefreshControl!
     
     var latestNewsList: [News] = []
     
@@ -25,13 +27,24 @@ class HomeViewController: UIViewController {
         // setup delegate
         tableView.delegate = self
         
+        let refreshControl = UIRefreshControl()
+        tableView.refreshControl = refreshControl
+        self.refreshControl = refreshControl
         
+        refreshControl.addTarget(self, action: #selector(self.refresh(_:)), for: .valueChanged)
+        
+        refreshControl.beginRefreshing()
         loadLatestNews()
     }
     
+    @objc func refresh(_ sender: Any) {
+        loadLatestNews()
+    }
 
+    // MARK: - Helpers
     func loadLatestNews() {
         ApiService.shared.loadLatestNews { result in
+            self.refreshControl.endRefreshing()
             switch result {
             case .success(let newsList):
                 self.latestNewsList = newsList
@@ -88,9 +101,17 @@ extension HomeViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let news = latestNewsList[indexPath.row]
         
-        let alert = UIAlertController(title: news.title, message: news.url, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Okay", style: .default))
+        // Showing alert
+//        let alert = UIAlertController(title: news.title, message: news.url, preferredStyle: .alert)
+//        alert.addAction(UIAlertAction(title: "Okay", style: .default))
+//
+//        present(alert, animated: true)
         
-        present(alert, animated: true)
+        if let url = URL(string: news.url) {
+            let controller = SFSafariViewController(url: url)
+            present(controller, animated: true)
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
